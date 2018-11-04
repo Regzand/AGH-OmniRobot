9a5c0c033d3064cc0d98b3355af8162a8da5cdc6
Jan Golda (Raspberry Pi) <jan.golda@gmail.com>
2018-11-04 20:11:57 +0100

    Fixed manual controll script

--- controll-manual.py
+++ controll-manual.py
@@ -1,3 +1,4 @@
+import math
 from MovementController import RetardedMovementController
 
 mc = RetardedMovementController()
@@ -6,15 +7,15 @@ print("Usage:\n", "\tS [-1, 1]\t speed\n", "\tD [0, 360)\t direction\n", "\tR [-
 
 while True:
 
-    cmd = input("> ").split(" ")
+    cmd = input("> ").lower().split(" ")
 
-    if cmd[0] == "S":
+    if cmd[0] == "s":
         mc.speed = float(cmd[1])
 
-    if cmd[0] == "D":
-        mc.direction = float(cmd[1])
+    if cmd[0] == "d":
+        mc.direction = float(cmd[1]) * 2 * math.pi / 360
 
-    if cmd[0] == "R":
+    if cmd[0] == "r":
         mc.rotation = float(cmd[1])
 
     if cmd[0] == "stop":
