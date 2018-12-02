ca99250a730db279d57d762dd5bd425f6b6b1bd3
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-12-02 18:20:32 +0100

    Added funny logs

--- robot/drivers/I2CDriver.py
+++ robot/drivers/I2CDriver.py
@@ -227,15 +227,20 @@ class I2CDriver:
 
         # addressing slave
         ack = self._send_byte_with_ack(write_address)
-        #print("device addressing ack: " + str(ack))
+        if ack == 1:
+            print("XDDDDDDDDDDDDDDDDDDD")
 
         # sending register address to slave
         ack = self._send_byte_with_ack(register_address)
+        if ack == 1:
+            print("XDDDDDDDDDDDDDDDDDDD")
         #print("device's register addressing ack: " + str(ack))
 
         # repeated start + reading register value from slave
         self._send_start_condition()
         ack = self._send_byte_with_ack(read_address)
+        if ack == 1:
+            print("XDDDDDDDDDDDDDDDDDDD")
         #print("device addressing ack: " + str(ack))
 
         byte = self._read_byte_with_ack()
